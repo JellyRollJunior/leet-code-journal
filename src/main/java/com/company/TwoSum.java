6858adf84406528f1d53f6004564a518a288b081
Brandon Lin <brandonlin9716@gmail.com>
2021-12-20 16:48:42 -0800

    Completed TwoSum.java reflection on Optimal Solution.

--- src/main/java/com/company/TwoSum.java
+++ src/main/java/com/company/TwoSum.java
@@ -1,10 +1,19 @@
 package com.company;
 
-import java.util.Arrays;
-import java.util.Collections;
+
 import java.util.HashMap;
 
 public class TwoSum {
+    /*
+     * Reflection: This question took me much longer than I would have liked. For my second solution, I played around
+     *             with the idea of sorting the array first but that would have given the solution N log N time at best.
+     *             Always think hash table first for best time.
+     *
+     * Optimal Solution Reflection: Always try to simplify code. I should have combined loops put the target check in
+     *                              the initial preprocessing loop like in the optimal solution. The optimal solution is
+     *                              also simpler than my solution as it doesn't need to deal with the duplicate value
+     *                              case.
+     */
 
     // initial brute force solution
     // Runtime: 91 ms, faster than 11.30% of Java online submissions for Two Sum.
@@ -47,6 +56,26 @@ public class TwoSum {
                 output[1] = index;
                 break;
             }
+        }
+
+        return output;
+    }
+
+    // Copying optimal solution
+    // Runtime: 1 ms, faster than 99.69% of Java online submissions for Two Sum.
+    // Memory Usage: 39 MB, less than 80.88% of Java online submissions for Two Sum.
+    public int[] twoSumOptimal(int[] nums, int target) {
+        int[] output = {0, 0};
+
+        // preprocess
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                output[1] = i;
+                output[0] = map.get(target - nums[i]);
+                return output;
+            }
+            map.put(nums[i], i);
         }
 
         return output;
