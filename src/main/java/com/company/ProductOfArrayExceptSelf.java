5ab4610f679bcb916d36b78de54110fe35c8ecd7
Brandon Lin <brandonlin9716@gmail.com>
2022-01-01 17:17:15 -0800

    Complete reflection and optimal solution analysis for ProductOfArrayExceptSelf.java.

--- src/main/java/com/company/ProductOfArrayExceptSelf.java
+++ src/main/java/com/company/ProductOfArrayExceptSelf.java
@@ -1,9 +1,13 @@
 package com.company;
 
 /* #238
- * Reflection:
+ * Reflection: I'm very happy I got the algorithm for the optimal solution. This was all because I wrote down some test
+ *             cases on a piece of scratch paper. I can feel myself learning and getting better!
  *
- * Optimal Solution Reflection:
+ * Optimal Solution Reflection: The optimal solution while having equal time complexity as mine, saves quite a bit of
+ *                              time and space due to not needing to store right side values. Instead, they multiply
+ *                              a variable. I should keep in mind when incrementally building a solution, It's not
+ *                              always necessary to save every intermediate value.
  *
  */
 public class ProductOfArrayExceptSelf {
@@ -27,7 +31,7 @@ public class ProductOfArrayExceptSelf {
         return result;
     }
 
-    // second attempt O(N-1 + N-1 + N) == O(3N) == O(N)
+    // second attempt (N-1) + (N-1) + N = 3N = O(N)
     // Runtime: 1 ms, faster than 100.00% of Java online submissions for Product of Array Except Self.
     // Memory Usage: 51.7 MB, less than 50.24% of Java online submissions for Product of Array Except Self.
     public int[] productExceptSelfSecond(int[] nums) {
@@ -52,7 +56,7 @@ public class ProductOfArrayExceptSelf {
         return result;
     }
 
-    // cleaned up previous solution
+    // cleaned up previous solution (N-1) + (N) = 2N-1 = O(N)
     // Runtime: 1 ms, faster than 100.00% of Java online submissions for Product of Array Except Self.
     // Memory Usage: 52.1 MB, less than 44.31% of Java online submissions for Product of Array Except Self.
     public int[] productExceptSelfThird(int[] nums) {
@@ -73,5 +77,21 @@ public class ProductOfArrayExceptSelf {
         result[0] = productMinusEnd[0];
 
         return result;
+    }
+
+    // Optimal solution found from discussion
+    public int[] productExceptSelfOPTIMAL(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        res[0] = 1;
+        for (int i = 1; i < n; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
+        }
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] *= right;
+            right *= nums[i];
+        }
+        return res;
     }
 }
