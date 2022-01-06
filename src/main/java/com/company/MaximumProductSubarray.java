04f04919dd40e39ad8dccd72493e12b26bd2e00f
Brandon Lin <brandonlin9716@gmail.com>
2022-01-05 17:25:23 -0800

    Complete refactored solution and reflection for MaximumProductSubarray.java.

--- src/main/java/com/company/MaximumProductSubarray.java
+++ src/main/java/com/company/MaximumProductSubarray.java
@@ -1,10 +1,17 @@
 package com.company;
 
+/* #152
+ * Reflection: Initial solution is slow and hacky due to all the if statements. For the second solution, I combined
+ *             some if statements and extracted reused code into a method. The algorithm is unchanged.
+ *
+ * Optimal Solution Reflection:
+ *
+ */
 public class MaximumProductSubarray {
 
     // initial 2N solution
-    // Runtime: 97 ms, faster than 5.33% of Java online submissions for Maximum Product Subarray.
-    // Memory Usage: 45.7 MB, less than 6.54% of Java online submissions for Maximum Product Subarray.
+    // Runtime: 2 ms, faster than 31.47% of Java online submissions for Maximum Product Subarray.
+    // Memory Usage: 41.9 MB, less than 6.54% of Java online submissions for Maximum Product Subarray.
     public int maxProduct(int[] nums) {
         int lastNegativeIndex = -1;
         int currentProduct = 1;
@@ -36,6 +43,53 @@ public class MaximumProductSubarray {
             } else if (nums[i] == 0) {
                 currentProduct = 1;
             }
+        }
+
+        return maxProduct;
+    }
+
+    // refactored solution
+    // Runtime: 1 ms, faster than 91.00% of Java online submissions for Maximum Product Subarray.
+    // Memory Usage: 41.7 MB, less than 6.54% of Java online submissions for Maximum Product Subarray.
+    public int maxProductSecond(int[] nums) {
+        int oddNegativeIndex = -1;
+        int currentProduct = 1;
+        int maxProduct = Integer.MIN_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (oddNegativeIndex < 0 && nums[i] < 0) {
+                oddNegativeIndex = i;
+            }
+
+            currentProduct = currentProduct * nums[i];
+            maxProduct = Math.max(maxProduct, currentProduct);
+
+            if (nums[i] == 0) {
+                currentProduct = 1;
+
+                if (oddNegativeIndex > -1) {
+                    int backtrackProduct = backtrack(nums, oddNegativeIndex + 1, i);
+                    maxProduct = Math.max(maxProduct, backtrackProduct);
+
+                    oddNegativeIndex = -1;
+                }
+            }
+        }
+
+        if (oddNegativeIndex > -1) {
+            int backtrackProduct = backtrack(nums, oddNegativeIndex + 1, nums.length - 1);
+            maxProduct = Math.max(maxProduct, backtrackProduct);
+        }
+
+        return maxProduct;
+    }
+
+    int backtrack(int[] nums, int start, int end) {
+        int maxProduct = Integer.MIN_VALUE;
+        int backtrackProduct = 1;
+        for (int i = start; i <= end; i++) {
+            backtrackProduct = backtrackProduct * nums[i];
+            maxProduct = Math.max(backtrackProduct, maxProduct);
         }
 
         return maxProduct;
