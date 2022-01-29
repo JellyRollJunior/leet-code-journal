53e90e90385a70d06f049f828cd7ba7507df0dce
Brandon Lin <brandonlin9716@gmail.com>
2022-01-28 16:55:17 -0800

    Complete initial solution for HouseRobber.java.

---
+++ src/main/java/com/company/HouseRobber.java
@@ -0,0 +1,29 @@
+package com.company;
+
+/* #198
+ * Reflection: My fastest Medium Yet! I might be a professional house robber. Although space could use some work.
+ *
+ * Optimal Solution Reflection:
+ *
+ */
+public class HouseRobber {
+
+    // initial solution : 20 minutes
+    // Runtime: 0 ms, faster than 100.00% of Java online submissions for House Robber.
+    // Memory Usage: 41.9 MB, less than 5.73% of Java online submissions for House Robber.
+    public int rob(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        } else if (nums.length == 2) {
+            return Math.max(nums[0], nums[1]);
+        } else {
+            int[] resultMatrix = new int[nums.length];
+            resultMatrix[0] = nums[0];
+            resultMatrix[1] = Math.max(nums[0], nums[1]);
+            for (int i = 2; i < nums.length; i++) {
+                resultMatrix[i] = Math.max(nums[i] + resultMatrix[i - 2], resultMatrix[i - 1]);
+            }
+            return Math.max(resultMatrix[nums.length - 1], resultMatrix[nums.length - 2]);
+        }
+    }
+}
