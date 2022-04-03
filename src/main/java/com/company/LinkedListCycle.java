f67b5123cfc78cab7d878349ca2dd987f71e213b
Brandon Lin <brandonlin9716@gmail.com>
2022-04-03 16:20:43 -0700

    Completed solutions for LinkedListCycle

---
+++ src/main/java/com/company/LinkedListCycle.java
@@ -0,0 +1,32 @@
+package com.company;
+
+/* #141
+ * Reflection: I only thought of a brute force O(N) space solution. lame. I thought of the optimal space solution once
+ *             I got the hint (basically the entire solution) see the pointer names walker and runner
+ *
+ * Optimal Solution Reflection: Thought linked lists would be easy but once again I'm naive. 8) discovered a neat way
+ *                              to use two pointers at least.
+ *
+ */
+public class LinkedListCycle {
+
+    // initial brute force solution is to store all mem addresses in a hash and see if its appeared already
+    // obviously wastes a lot of space
+
+    // optimal solution
+    // Runtime: 0 ms, faster than 100.00% of Java online submissions for Linked List Cycle.
+    // Memory Usage: 45.2 MB, less than 79.28% of Java online submissions for Linked List Cycle.
+    public boolean hasCycle(ListNode head) {
+        ListNode runner = head;
+        ListNode walker = head;
+        while (runner != null && runner.next != null) {
+            runner = runner.next.next;
+            walker = walker.next;
+            if (runner == walker) return true;
+        }
+        return false;
+    }
+
+    // proof: two cases N == odd, N == even
+        // insert proof that while loop condition always terminates or returns
+}
--- src/main/java/com/company/ReverseLinkedList.java
+++ src/main/java/com/company/ReverseLinkedList.java
@@ -11,23 +11,6 @@ package com.company;
  */
 public class ReverseLinkedList {
 
-    public class ListNode {
-        int val;
-        ListNode next;
-
-        ListNode() {
-        }
-
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
-    }
-
     // initial iterative solution O(N) space/time
     // Runtime: 0 ms, faster than 100.00% of Java online submissions for Reverse Linked List.
     // Memory Usage: 43.3 MB, less than 21.45% of Java online submissions for Reverse Linked List
